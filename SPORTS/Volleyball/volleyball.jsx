c362695b41504d71895eab7c4c72767ee17988ec
Ajingolia <ajingolia@gmail.com>
2024-02-07 15:24:44 -0600

    adding files

---
+++ SPORTS/Volleyball/volleyball.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+export const Label = () => {
+  return (
+    <div className="label">
+      <p className="location-okun-field">
+        Location: Okun Field-house
+        <br />
+        Court: Raven 4<br />
+        Event: Okun Summer league
+        <br />
+        Start Time: 12:00PM CST
+      </p>
+    </div>
+  );
+};
